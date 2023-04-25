d4126a51ddc1df7e9ad8bcd3f915f9510091ebca
Gabriel Taillon <ga.taillon@protonmail.com>
2023-04-25 23:06:23 +0900

    [add] build_targets

--- mace.h
+++ mace.h
@@ -1,6 +1,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -25,8 +26,9 @@ struct Target {
     int   kind;
 
     /* Private members */
-    char **_sources;         /* filenames */
+    char  **_sources;         /* filenames */
     size_t  _sources_num;
+    char  *_name;
 };
 
 /* --- EXAMPLE TARGET --- */
@@ -53,7 +55,14 @@ struct PHONY {
 // 1- Saves target name hash
 // 2- Builds list of sources
 // All added targets are built
-#define MACE_ADD_TARGET(a)
+#define MACE_ADD_TARGET(target) do {\
+targets[target_num] = target;\
+if (++target_num == target_len) {\
+    target_len *= 2;\
+    targets = realloc(targets, target_len * sizeof(*targets));\
+}\
+target._name = #target;\
+}while(0)
 
 /******************************** MACE_ADD_PHONY *******************************/
 // Phony targets are only built when called explicitely e.g. <./build> install
@@ -101,52 +110,67 @@ void mace_target_dependency(struct Target *targets, size_t len) {
 
 /********************************* mace_build **********************************/
 /* Cuild all sources from target to object */
-void mace_build(struct Target *target, size_t len) {
+void mace_build_targets(struct Target *targets, size_t len) {
+
+}
+
+void mace_build_target(struct Target *target) {
+
+
+    /* --- Linking --- */
+    printf("Linking %s", target->_name);
     if (target->kind == MACE_LIBRARY) {
-
-    } else if { (target->kind == MACE_LIBRARY)
-              }
-
-    LINKCMD = $(AR) - rcs "$@" $(OBJECTS)
-        void mace_link(char *objects, char *target) {
-        char *arguments[] = {ar, "-rcs", target, objects};
-        printf("Linking  %d\n", target);
-        execvp(ar, arguments);
+        // mace_link(objects, target);
+    } else if (target->kind == MACE_EXECUTABLE) {
+        // mace_compile(char *source, char *object, char *flags);
     }
 
+}
 
-    /* Compile a single source file to object */
-    void mace_compile(char *source, char *object, char *flags) {
-        char *arguments[] = {cc, source, "-o", object, flags};
-        printf("%s\n", source);
-        execvp(cc, arguments);
-    }
+void mace_link(char *objects, char *target) {
+    char *arguments[] = {ar, "-rcs", target, objects};
+    printf("Linking  %d\n", target);
+    execvp(ar, arguments);
+}
 
-    /************************************ mace *************************************/
-    // User-implemented function.
-    // SHOULD:
-    // 1- Set compiler
-    // 2- Add targets
-    extern int mace(int argc, char *argv[]);
 
-    /************************************ main ************************************/
-    // 1- Run mace, get all info from user:
-    //   a- Get compiler
-    //   a- Get targets
-    // 2- Build dependency graph from targets
-    // 3- Determine which targets need to be recompiled
-    // 4- Build the targets
+/* Compile a single source file to object */
+void mace_compile(char *source, char *object, char *flags) {
+    char *arguments[] = {cc, source, "-o", object, flags};
+    printf("%s\n", source);
+    execvp(cc, arguments);
+}
 
-    // if `mace clean` is called (clean target), rm all targets
+/************************************ mace *************************************/
+// User-implemented function.
+// SHOULD:
+// 1- Set compiler
+// 2- Add targets
+extern int mace(int argc, char *argv[]);
 
-    struct Target *targets;
+/************************************ main ************************************/
+// 1- Run mace, get all info from user:
+//   a- Get compiler
+//   a- Get targets
+// 2- Build dependency graph from targets
+// 3- Determine which targets need to be recompiled
+// 4- Build the targets
 
-    int main(int argc, char *argv[]) {
-        mace(argc, argv);
-        size_t len = 0;
-        mace_target_dependency(targets, len);
-        // mace_compile(arguments);
-        mace_compile("mace.c", "baka.out", NULL);
-        mace_build(targets, len);
-        return (0);
-    }
+// if `mace clean` is called (clean target), rm all targets
+
+struct Target *targets = NULL;
+size_t target_num = 0;
+size_t target_len = 2;
+
+int main(int argc, char *argv[]) {
+    /* --- Preliminaries --- */
+    targets = malloc(target_len * sizeof(*targets));
+
+    mace(argc, argv);
+    size_t len = 0;
+    mace_target_dependency(targets, len);
+    // mace_compile(arguments);
+    mace_compile("mace.c", "baka.out", NULL);
+    mace_build_targets(targets, target_num);
+    return (0);
+}
