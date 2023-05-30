c839e9aea91131983b5ef63813303e879afcb957
Gabriel Taillon <ga.taillon@protonmail.com>
2023-05-30 23:54:43 +0900

    [add] buildallatonce

--- mace.c
+++ mace.c
@@ -38,6 +38,7 @@ struct Target cJson = { /* Unitialized values guaranteed to be 0 / NULL */
 struct Target physfs = { /* Unitialized values guaranteed to be 0 / NULL */
     .sources            = ".",
     .base_dir           = "third_party/physfs",
+    .allatonce          = false,
     .kind               = MACE_STATIC_LIBRARY,
 };
 
@@ -47,15 +48,15 @@ struct Target tinymt = { /* Unitialized values guaranteed to be 0 / NULL */
     .kind               = MACE_STATIC_LIBRARY,
 };
 
-struct Target CodenameFiresaga = { /* Unitialized values guaranteed to be 0 / NULL */
-    .includes           = ". include  include/bars  include/menu include/popup"
+struct Target SotA = { /* Unitialized values guaranteed to be 0 / NULL */
+    .includes           = ". include include/bars include/menu include/popup " 
                           "include/systems names names/popup names/menu "
-                          "second_party/nstr second_party/noursmath second_party/tnecs "
+                          "second_party/nstr second_party/noursmath second_party/tnecs " 
                           "second_party/parg second_party/nourstest "
-                          "third_party/physfs third_party/tinymt third_party/stb "
+                          "third_party/physfs third_party/tinymt third_party/stb " 
                           "third_party/cJson",
     .sources            = "src/*.c src/bars/ src/menu/ src/popup/ src/systems/ src/game/",
-    .links              = "SDL2 SDL2_image SDL2_ttf m GLEW cJson noursmath physfs "
+    .links              = "SDL2 SDL2_image SDL2_ttf m GLEW cJson noursmath physfs " 
                           "tinymt tnecs nstr parg",
     .kind               = MACE_EXECUTABLE,
 };
@@ -69,18 +70,16 @@ int mace(int argc, char *argv[]) {
     MACE_SET_COMPILER(CC);
     mace_set_build_dir("A_build");
     mace_set_obj_dir("B_obj");
-    
     /* - second_party - */
-    MACE_ADD_TARGET(noursmath);
-    MACE_ADD_TARGET(nstr);
-    MACE_ADD_TARGET(parg);
-    MACE_ADD_TARGET(tnecs);
-    
+    // MACE_ADD_TARGET(noursmath);
+    // MACE_ADD_TARGET(nstr);
+    // MACE_ADD_TARGET(parg);
+    // MACE_ADD_TARGET(tnecs);
     /* - third_party - */
-    MACE_ADD_TARGET(cJson);
-    MACE_ADD_TARGET(physfs);
-    MACE_ADD_TARGET(tinymt);
-
-    MACE_ADD_TARGET(CodenameFiresaga);
-    return (0);
+    // MACE_ADD_TARGET(cJson);
+    // MACE_ADD_TARGET(physfs);
+    // MACE_ADD_TARGET(tinymt);
+    
+    /* - SotA - */
+    MACE_ADD_TARGET(SotA);
 }
