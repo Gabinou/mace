bcb60971b424a9414f6dd77599d6b73376bbeab2
Gabriel Taillon <ga.taillon@protonmail.com>
2023-05-30 23:54:44 +0900

    [clean]

--- mace.c
+++ mace.c
@@ -74,7 +74,8 @@ int main(int argc, char *argv[]) {
     int len             = 8;
     int argc_compile    = 0;
     char **argv_compile = calloc(len, sizeof(*argv_compile));
-    argv_compile = mace_argv_flags(&len, &argc_compile, argv_compile, compile_cmd, NULL, false, mace_flag_separator);
+    argv_compile = mace_argv_flags(&len, &argc_compile, argv_compile, compile_cmd, NULL, false,
+                                   mace_flag_separator);
 
     /* - Compile it - */
     mace_exec_print(argv_compile, argc_compile);
--- style.sh
+++ style.sh
@@ -4,4 +4,4 @@
 # Copyright (C) Gabriel Taillon, 2023
 # Automatic formatting with astyle for mace 
 
-astyle --options=style.txt --verbose test.c mace.c mace.h installer.c
+astyle --options=style.txt --verbose test/test.c mace.c mace.h installer.c
